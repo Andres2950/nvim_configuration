3c9a862eaaf86137b8c769054619c49334e5f752
Andres2950 <andres.umana777@gmail.com>
2025-09-14 21:50:23 -0600

    Delete nvim directory

--- nvim/init.lua
+++
@@ -1,3 +0,0 @@
-require("config.lazy")
-require("config.nvim_settings")
-require("config.keymaps")
--- nvim/lua/config/nvim_settings.lua
+++
@@ -1,9 +0,0 @@
--- Lineas de numero
-vim.o.number = true
-vim.o.relativenumber = true
-
--- 4 espacios para el tab
-vim.o.tabstop = 4
-vim.o.shiftwidth = 4
-vim.o.softtabstop = 4
-vim.o.expandtab = true
