f6fa9bc35e7d1d89313dc4b385655332b161d97f
OscarEl+Prron <osobando@estudiantec.cr>
2025-09-17 16:13:47 -0600

    MD preview kemaps

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -4,3 +4,7 @@ vim.keymap.set("n", "<leader>bd", "<cmd>bd<CR>", { desc = "Buffer Delete" })
 vim.keymap.set("n", "<leader>bn", "<cmd>bn<CR>", { desc = "Buffer Next" })
 vim.keymap.set("n", "<leader>bp", "<cmd>bp<CR>", { desc = "Buffer Previous" })
 vim.keymap.set("n", "<leader>w", "<cmd>w<CR>", { desc = "Write file" })
+
+vim.keymap.set("n", "<leader>mp", "<cmd>MarkdownPreview<CR>", { desc = "Markdown Preview"})
+vim.keymap.set("n", "<leader>mps", "<cmd>MarkdownPreviewStop<CR>", { desc = "Markdown Preview Stop"})
+vim.keymap.set("n", "<leader>mpt", "<cmd>MarkdownPreviewToggle<CR>", { desc = "Markdown Preview Toggle"})
